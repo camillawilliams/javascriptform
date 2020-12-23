459e1d3ba5b35532fc2bce766bc7c83254b195c4
camillawilliams <camillawilliamsdu@gmail.com>
2020-12-23 23:58:11 +0000

    fixed error

--- src/main.js
+++ src/main.js
@@ -21,30 +21,31 @@ const errorAlert = [
 //   console.log("Hello Rigo from the console!");
 // };
 
-const allInputs = document.querySelectorAll("form-control");
+const allInputs = document.querySelector("form-control");
+// const cardnumber = document.querySelector("#cardnumber");
 
 function validation() {
-  var cardnumber = document.querySelector("cardnumber");
-  var ccv = document.querySelector("ccv");
-  var amount = document.querySelector("amount");
-  var firstname = document.querySelector("firstname");
-  var lastname = document.querySelector("lastname");
-  var city = document.querySelector("city");
-  var state = document.querySelector("state");
-  var zip = document.querySelector("zip");
+  var cardnumber = document.querySelector("#cardnumber");
+  var ccv = document.querySelector("#ccv");
+  var amount = document.querySelector("#amount");
+  var firstname = document.querySelector("#firstname");
+  var lastname = document.querySelector("#lastname");
+  var city = document.querySelector("#city");
+  var state = document.querySelector("#state");
+  var zipcode = document.querySelector("#zip");
 }
 
-if (cardnumber.value.length < 10000000000000000) {
+if (cardnumber.value < 10000000000000000) {
   displayError("Card number must be 16 digits");
   cardnumber.classList.add("invalid");
 }
-if (CCV.value.length != 3) {
+if (ccv.value.length != 3) {
   displayError("CCV must be 16 digits");
-  CCV.classList.add("invalid");
+  ccv.classList.add("invalid");
 }
-if (amount.value.length > 0) {
-  displayError("Amount must be entered");
-  CCV.classList.add("invalid");
+if (amount.value.length != 0) {
+  displayError("Amount required");
+  amount.classList.add("invalid");
 }
 if (firstname.value.length > 0) {
   displayError("First name must be entered");
@@ -68,7 +69,7 @@ if (zipcode.value != 5) {
 }
 
 function displayError(message) {
-  var errorField = document.querySelector("alert");
+  var errorField = document.querySelector(".alert");
   errorField.innerHTML += `<p>${message}</p>`;
 }
 document.querySelector("#myform").addEventListener("submit", validation);
